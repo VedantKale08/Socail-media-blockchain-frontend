3fa22a0ab24758bb7602a73e8353b7e06e47dc9e
VedantKale08 <fscocomputer@gmail.com>
2024-03-22 15:11:33 +0530

    changes in register file

--- components/Landing/Register.jsx
+++ components/Landing/Register.jsx
@@ -1,10 +1,14 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 
 const Register = () => {
-  return <div className="h-screen overflow-hidden bg-black">Register</div>;
+  return (
+    <div className="h-screen overflow-hidden bg-black flex justify-center items-center text-white">
+      <div className="bg-[#16181c] w-[30vw] h-fit border border-slate-500 rounded-md py-4">
+        <p className="text-center text-2xl font-bold">DenZ</p>
+      </div>
+    </div>
+  );
+};
 
-  //   16181c
-}
-
-export default Register
+export default Register;
