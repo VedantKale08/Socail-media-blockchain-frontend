0af1ebcfa0eb297cf995586022ee51dd058f67ec
DevAniket010 <fs20co010@gmail.com>
2024-03-22 15:30:28 +0530

    landing half complete

--- components/Landing/Landing.jsx
+++ components/Landing/Landing.jsx
@@ -1,10 +1,34 @@
+'use client'
 import React from 'react'
+import Lottie from 'react-lottie'
+import * as animationData from '@/public/assets/lottie/landing.json'
+import { Button } from '@mui/material'
 
 const Landing = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
   return (
     <>
-    <div className="h-screen overflow-hidden bg-black">
-        
+    <div className="h-screen overflow-hidden bg-black flex items-center flex-col">
+    <div>
+      <Lottie 
+	    options={defaultOptions}
+        height={600}
+        width={600}
+      />
+    </div>
+    <div className='text-white font-bold text-4xl mt-10'>
+        #Join the DenZ Revolution
+    </div>
+    <div className='mt-10'>
+    <Button style={{ backgroundColor: 'rgb(59, 130, 246)', color: 'white' ,paddingLeft:'70px' , paddingRight:'70px', paddingTop:'10px',paddingBottom:'10px',borderRadius:'20px',fontSize:'20px',fontWeight:'bold'}}>Connect</Button>
+    </div>
         </div>
     </>
   )
