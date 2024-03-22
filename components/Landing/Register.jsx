5231dd5223f43a6dde195a418e5818e7f6acb670
VedantKale08 <fscocomputer@gmail.com>
2024-03-22 14:45:55 +0530

    work in progress

---
+++ components/Landing/Register.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+const Register = () => {
+  return <div className="h-screen overflow-hidden bg-black">Register</div>;
+
+  //   16181c
+}
+
+export default Register
