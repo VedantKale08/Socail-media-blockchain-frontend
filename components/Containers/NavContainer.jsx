b0d6750c1a79c8f16dc9ba45a4b5b94412691e3f
VedantKale08 <fscocomputer@gmail.com>
2024-03-22 22:24:52 +0530

    profile done

--- components/Containers/NavContainer.jsx
+++ components/Containers/NavContainer.jsx
@@ -5,6 +5,7 @@ import SuggestionSection from '../Home/SuggestionSection'
 import { tabsStore } from '@/store/tabState';
 import PostSection from '../Home/PostSection';
 import Trending from '../Home/Trending';
+import Profile from '../Home/Profile';
 
 const NavContainer = () => {
   const tab = tabsStore((state) => state.tab);
@@ -14,7 +15,7 @@ const NavContainer = () => {
       <div className="grid grid-cols-[1fr,350px] px-14 py-8 bg-gray-200 h-[calc(100vh-90px)] gap-14 overflow-scroll">
         {tab === "Home" && <PostSection/>}
         {tab === "Trending" && <Trending/>}
-        {tab === "Profile" && <p>Profile</p>}
+        {tab === "Profile" && <Profile/>}
         {tab === "Settings" && <p>Settings</p>}
         <SuggestionSection />
       </div>
